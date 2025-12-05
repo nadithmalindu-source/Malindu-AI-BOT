e91fb4c1518e66cbdd789ae13880d65ed61362e4
nadithmalindu-source <nadithmalindu@gmail.com>
2025-12-05 13:51:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/nadithmalindu-source/Malindu-AI-BOT/blob/main/image/Screenshot%202025-12-05%20133333.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MALINDU AI BOT Is Alive Now😍*",
+BOT_OWNER: '94701369636',  // Replace with the owner's phone number
 
 
 
