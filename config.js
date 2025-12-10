dd29b1cbb7f9a09812d69292c539227cde968523
nadithmalindu-source <nadithmalindu@gmail.com>
2025-12-10 21:10:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SIMWSQ5Q#WtB8Enp5HyTwuCKu--V7BqdDWsUz-8UCTqjjxOz1YF0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/nadithmalindu-source/Malindu-AI-BOT/blob/main/image/Gemini_Generated_Image_unjbleunjbleunjb.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MALIYA-MD BOT Is Alive Now😍*",
 BOT_OWNER: '94701369636',  // Replace with the owner's phone number
